1568c630848b87ec5474140f2014b2449246ecfd
Kevin Chen & Peter <student@epicodus.com>
2015-07-13 15:48:45 -0700

    able to up vote and down vote

---
+++ app/controllers/options_controller.rb
@@ -0,0 +1,20 @@
+class OptionsController < ApplicationController
+  def update
+    @question = Question.find(params[:question_id])
+    @option = @question.options.find(params[:id])
+    if @option.update(score: @option.score + 1)
+      respond_to do |format|
+        format.html { redirect_to questions_path }
+        format.js
+      end
+    end
+  end
+
+# because we are only updating score we can just use an update on the score. instead of embedding it into the link
+
+  private
+
+  def option_params
+    params.require(:option).permit(:description, :score)
+  end
+end
